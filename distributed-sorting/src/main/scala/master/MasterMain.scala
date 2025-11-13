5c0cac25eb6e6a806f310746e1adbd7ce3db6bb1
saltyvive <junymath0530@naver.com>
2025-11-13 19:31:34 +0900

    feat: MasterMain to be used for test is implemented

--- distributed-sorting/src/main/scala/master/MasterMain.scala
+++ distributed-sorting/src/main/scala/master/MasterMain.scala
@@ -1,3 +1,24 @@
+import com.master.server.MasterServer.MasterServerGrpc
+import io.grpc.netty.NettyServerBuilder
+import scala.concurrent.{ExecutionContext}
+import scala.concurrent.ExecutionContextExecutor
+
+
 object MasterMain extends App {
-    println("Hello, World!")
+    implicit val ec:ExecutionContext = ExecutionContext.global
+
+    private val port = 50051
+
+    val server = NettyServerBuilder
+    .forPort(port)
+    .addService(MasterServerGrpc.bindService(new MasterServerImpl, ec))
+    .build.start()
+
+    println(s"[Master] Server Started on port: $port")
+
+    sys.addShutdownHook{
+        println("[Master] Server Shutdown...")
+        server.shutdown()
+    }
+
 }
