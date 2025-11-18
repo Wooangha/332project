c6aac5f403fb1888ddf7990500cce6b82b2bd41f
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-11-18 22:51:11 +0900

    feat: implement sampling, sort, partitioning and manage temp directory

--- distributed-sorting/src/main/scala/worker/io/DataProcessor.scala
+++ distributed-sorting/src/main/scala/worker/io/DataProcessor.scala
@@ -3,9 +3,47 @@ package worker.io
 import java.nio.file.{Paths, Files}
 
 import scala.collection.mutable
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object  DataProcessor {
-  
+  val tempDirPrefix = "tmpSave/"
+
+  def sampling(dataDirLs: List[String], sampleSize: Int): Future[Vector[Key]] = {
+    val sampleSizePerFile = Math.ceil(sampleSize.toDouble / dataDirLs.length.toDouble).toInt
+
+    val futures = for (dir <- dataDirLs)
+      yield Future {
+        Data.fromFile(dir).sampling(sampleSizePerFile)
+      }
+
+    Future.sequence(futures).map(_.toVector.flatten)
+  }
+
+  def sortAndPartitioning(
+      dataDirLs: List[String],
+      partition: Key => String): Future[Unit] = {
+
+    val futures = for ((inputDir, numOfData) <- dataDirLs.zip(0 until dataDirLs.length))
+      yield Future {
+        val data = Data.fromFile(inputDir).sort().partitioning(partition)
+        for ((ip, partData) <- data) Future {
+          partData.save(tempDirPrefix + ip + "-" + numOfData.toString)
+        }
+      }
+
+    Future.sequence(futures).map(_ => ())
+  }
+
+  def removeTempDir(): Unit = {
+    val tempDirPath = Paths.get(tempDirPrefix)
+    if (Files.exists(tempDirPath)) {
+      Files.walk(tempDirPath)
+        .sorted(java.util.Comparator.reverseOrder())
+        .forEach(path => Files.delete(path))
+    }
+  }
+
   def merge(dataDirLs: List[String], makeNewDir: () => String, maxSize: Int): Unit = {
     val dataIters = dataDirLs.map(dir => new DatumFileIterator(dir))
 
