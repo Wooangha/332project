b460f0a2c30bddc83339fa20eab7a7a2e4238e1f
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-11-18 22:45:46 +0900

    test: update IOSuite to compare data correctly and divide SortSuite for sorting tests

--- distributed-sorting/src/test/scala/worker/io/IOSuite.scala
+++ distributed-sorting/src/test/scala/worker/io/IOSuite.scala
@@ -16,7 +16,7 @@ class IOSuite extends AnyFunSuite {
 
     assert {
       val newData = Data.fromFile(outputDir)
-      newData === data
+      newData.data === data.data
     }
   }
 
@@ -31,31 +31,6 @@ class IOSuite extends AnyFunSuite {
     assert {
       val newData = Data.fromFile(outputDir)
       newData === data
-    }
-  }
-}
-
-class SortSuite extends AnyFunSuite {
-
-  test("Sort data read from a file and write it back to another file — the output file should be sorted.") {
-    val inputDir = "src/test/resources/ascii_data"
-    val inputSortedDir = "src/test/resources/sorted_ascii_data"
-    val outputDir = "src/test/resources/output/sorted_out"
-
-    val data = Data.fromFile(inputDir)
-
-    val sortedData = data.sort()
-
-    sortedData.save(outputDir)
-    val newData = Data.fromFile(outputDir)
-
-    val answerData = Data.fromFile(inputSortedDir)
-    answerData.data.zip(newData.data).foreach { case (expected, actual) => 
-      assert(expected.key === actual.key && expected.value === actual.value)
-    }
-
-    assert {
-      newData.data.length === (answerData.data.length)
     }
   }
 }
---
+++ distributed-sorting/src/test/scala/worker/io/SortSuite.scala
@@ -0,0 +1,28 @@
+package worker.io
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class SortSuite extends AnyFunSuite {
+
+  test("Sort data read from a file and write it back to another file — the output file should be sorted.") {
+    val inputDir = "src/test/resources/ascii_data"
+    val inputSortedDir = "src/test/resources/sorted_ascii_data"
+    val outputDir = "src/test/resources/output/sorted_out"
+
+    val data = Data.fromFile(inputDir)
+
+    val sortedData = data.sort()
+
+    sortedData.save(outputDir)
+    val newData = Data.fromFile(outputDir)
+
+    val answerData = Data.fromFile(inputSortedDir)
+    answerData.data.zip(newData.data).foreach { case (expected, actual) => 
+      assert(expected.key === actual.key && expected.value === actual.value)
+    }
+
+    assert {
+      newData.data.length === (answerData.data.length)
+    }
+  }
+}
