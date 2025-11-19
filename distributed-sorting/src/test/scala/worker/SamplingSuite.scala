78a36c9d28363dcc76162a7748f960066d6bf148
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-11-19 16:06:54 +0900

    test: add SamplingSuite for data sampling tests

---
+++ distributed-sorting/src/test/scala/worker/SamplingSuite.scala
@@ -0,0 +1,38 @@
+package worker
+
+import java.nio.file.{Paths, Files}
+
+import scala.concurrent.Await
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import common.Data
+import worker.DataProcessor.sampling
+
+class SamplingSuite extends AnyFunSuite with GenData with CheckSorted {
+
+  test("Sample data from a file — samples exist in the data") {
+    val inputDirs = List("src/test/resources/partition1", "src/test/resources/partition2", "src/test/resources/partition3")
+
+    val partitionSize = 100000
+
+    for ((inputDir, index) <- inputDirs.zip(0 until inputDirs.length))
+      generateData(inputDir, index * partitionSize, partitionSize, skewed = false)
+
+    val keys = Await.result(sampling(inputDirs, 10000), scala.concurrent.duration.Duration.Inf)
+
+    val datas = inputDirs.map { dir => 
+      Data.fromFile(dir).data.map(_.toTuple).toMap
+    }
+
+    keys.foreach { key =>
+      assert {
+        datas.exists { dataMap =>
+          dataMap.contains(key)
+        }
+      }
+    }
+
+    cleanupGeneratedData()
+  }
+}
