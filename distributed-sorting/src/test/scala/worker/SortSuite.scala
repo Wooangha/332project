4ab1145d4c18df47277ba1c0dcb73d6f8d3cbaa8
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-11-19 15:33:33 +0900

    fix: update input directory path in SortSuite test

--- distributed-sorting/src/test/scala/worker/SortSuite.scala
+++ distributed-sorting/src/test/scala/worker/SortSuite.scala
@@ -11,7 +11,7 @@ import common.Data
 class SortSuite extends AnyFunSuite with GenData with CheckSorted {
 
   test("Sort data read from a file and write it back to another file — the output file should be sorted.") {
-    val inputDir = "src/test/resources/ascii_data"
+    val inputDir = "src/test/resources/data"
     val outputDir = "src/test/resources/output/sorted_out"
 
     generateData(inputDir, 10, 1000, skewed = false)
