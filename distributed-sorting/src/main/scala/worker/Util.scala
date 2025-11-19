1e8252567e753e1e6cae227a484c869af70ab8a0
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-11-19 17:23:50 +0900

    feat: implement makePartition function for partitioning logic

---
+++ distributed-sorting/src/main/scala/worker/Util.scala
@@ -0,0 +1,37 @@
+package worker
+
+import java.security.KeyException
+
+import common.Key
+
+object Util {
+
+  /**
+    * Make a partition function from partition ranges and worker infos
+    *
+    * @param partitionRange
+    * @param workerInfos
+    * @return
+    * @throws KeyException if no partition is found for a key
+    */
+  def makePartition(partitionRange: List[(Key, Key)], workerInfos: List[String]): Key => String = {
+    val sortedRanges = partitionRange.sortBy(_._1)
+    val sortedWorkerInfos = workerInfos.sorted
+
+    key: Key => {
+      if (key == Key.max) {
+        sortedWorkerInfos.last
+      } else {
+        val idx = sortedRanges.indexWhere { case (start, end) =>
+          key >= start && key < end
+        }
+        if (idx == -1) {
+          throw new KeyException(s"No partition found for key: ${key}, now partitionRanges: ${partitionRange.toString()}")
+        } else {
+          sortedWorkerInfos(idx)
+        }
+      }
+    }
+  }
+
+}
