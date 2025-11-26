b6e8e8b45f644011108e820984046ea2c6b5a886
saltyvive <junymath0530@naver.com>
2025-11-26 23:19:16 +0900

    fix : MasterTestMain file is fixed for changed MasterServerImpl argument

--- distributed-sorting/src/test/scala/master/MasterTestMain.scala
+++ distributed-sorting/src/test/scala/master/MasterTestMain.scala
@@ -1,6 +1,6 @@
 import com.master.server.MasterServer.MasterServerGrpc
 import io.grpc.netty.NettyServerBuilder
-import scala.concurrent.{ExecutionContext}
+import scala.concurrent.{ExecutionContext, Promise}
 import scala.concurrent.ExecutionContextExecutor
 
 object MasterTestMain extends App {
@@ -8,10 +8,12 @@ object MasterTestMain extends App {
 
     private val port = 50057
 
+    val masterShutdownPromise = Promise[Unit]()
+
     val server = NettyServerBuilder
-    .forPort(port)
-    .addService(MasterServerGrpc.bindService(new MasterServerImpl(3), ec))
-    .build.start()
+        .forPort(port)
+        .addService(MasterServerGrpc.bindService(new MasterServerImpl(3, onMasterCanShutdown = () => masterShutdownPromise.trySuccess(())), ec))
+        .build.start()
 
     println(s"[Master] Server Started on port: $port")
 
@@ -21,6 +23,10 @@ object MasterTestMain extends App {
         server.shutdown()
     }
     */
+
+    masterShutdownPromise.future.foreach { _ =>
+        server.shutdown()
+    }
 
     server.awaitTermination()
 }
